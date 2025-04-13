dc5fb854b1dc169417016a4ffd2c222a5e2ab7c0
bluefoundry <bluefoundry@proton.me>
2025-04-14 04:21:44 +0900

    prep initial release (#1)
    * prep initial release
    
    * change CI actions to use v0
    
    * updated to fq tags
    
    * fixed action versions
    
    * fixed error in makefile
    
    ---------
    
    Co-authored-by: bluefoundry <bluefoundry@noreply.com>

---
+++ config.go
@@ -0,0 +1,193 @@
+package envconfig
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type EnvConfigOption interface {
+	Name() string
+	Props() map[string]any
+}
+
+type Options struct {
+	// errorOnMissing will cause Apply to return an error if an environment variable is missing and has no default value
+	errorOnMissing bool
+}
+
+type ConfigOption func(*Options)
+
+// WithErrorOnMissing will cause the Apply function to return an error if any environment config is missing and has no default value
+func WithErrorOnMissing() ConfigOption {
+	return func(o *Options) {
+		o.errorOnMissing = true
+	}
+}
+
+func setDuration(fv reflect.Value, value string) error {
+	var scalarPortion string
+	var typePortion string
+	for i := 0; i < len(value); i++ {
+		if value[i] < 48 || value[i] > 57 {
+			scalarPortion = value[:i]
+			typePortion = value[i:]
+			break
+		}
+	}
+	s, err := strconv.ParseInt(scalarPortion, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	var v int64
+
+	switch typePortion {
+	case "h":
+		v = int64(time.Hour) * s
+	case "m":
+		v = int64(time.Minute) * s
+	case "s":
+		v = int64(time.Second) * s
+	case "ms":
+		v = int64(time.Millisecond) * s
+	case "us":
+		v = int64(time.Microsecond) * s
+	case "ns":
+		v = int64(time.Nanosecond) * s
+	case "d":
+		v = int64(time.Hour) * 24 * s
+	case "w":
+		v = int64(time.Hour) * 24 * 7 * s
+	default:
+		return fmt.Errorf("duration string had unknown value: '%s'", typePortion)
+	}
+
+	fv.SetInt(v)
+	return nil
+}
+
+func setInt(fv reflect.Value, value string) error {
+	v, err := strconv.ParseInt(value, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	fv.SetInt(v)
+	return nil
+}
+
+func setFloat(fv reflect.Value, value string) error {
+	v, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		return err
+	}
+
+	fv.SetFloat(v)
+	return nil
+}
+
+func setBool(fv reflect.Value, value string) error {
+	var bv bool
+	lv := strings.ToLower(value)
+	switch lv {
+	case "t", "true", "1":
+		bv = true
+	case "f", "false", "0":
+		bv = false
+	default:
+		return fmt.Errorf("string was not clearly representative of a boolean value")
+	}
+
+	fv.SetBool(bv)
+	return nil
+}
+
+func applyValue(fv reflect.Value, envValue string) error {
+	var err error
+	switch fv.Kind() {
+	case reflect.Int:
+		err = setInt(fv, envValue)
+	case reflect.Int64:
+		ifx := fv.Interface()
+		switch ifx.(type) {
+		case time.Duration:
+			err = setDuration(fv, envValue)
+		case int64:
+			err = setInt(fv, envValue)
+		default:
+			return fmt.Errorf("unknown interface type built on int64")
+		}
+	case reflect.Int32:
+		err = setInt(fv, envValue)
+	case reflect.Int16:
+		err = setInt(fv, envValue)
+	case reflect.Int8:
+		err = setInt(fv, envValue)
+	case reflect.Float64:
+		err = setFloat(fv, envValue)
+	case reflect.Float32:
+		err = setFloat(fv, envValue)
+	case reflect.String:
+		fv.SetString(envValue)
+	case reflect.Bool:
+		err = setBool(fv, envValue)
+	default:
+		return fmt.Errorf("unexpected field type %s", fv.Kind().String())
+	}
+
+	return err
+}
+
+func Apply(target any, options ...ConfigOption) error {
+	opt := &Options{}
+	for _, o := range options {
+		o(opt)
+	}
+
+	t := reflect.TypeOf(target)
+	v := reflect.ValueOf(target)
+	k := t.Kind()
+	if k != reflect.Pointer {
+		return fmt.Errorf("target must be a pointer type")
+	}
+	e := t.Elem()
+	ve := v.Elem()
+	k = e.Kind()
+	if k != reflect.Struct {
+		return fmt.Errorf("target must point to a struct type")
+	}
+
+	for i := 0; i < e.NumField(); i++ {
+		f := ve.Field(i)
+		v := e.Field(i)
+		tag := v.Tag
+		envVar := tag.Get("env")
+		if envVar == "" {
+			continue
+		}
+
+		defaultVal := tag.Get("default")
+
+		envValue := os.Getenv(envVar)
+		if envValue == "" {
+			envValue = defaultVal
+		}
+
+		if opt.errorOnMissing && envValue == "" {
+			return fmt.Errorf("env var %s was not provided and has no default", envValue)
+		}
+		if envValue != "" {
+			err := applyValue(f, envValue)
+			if err != nil {
+				return fmt.Errorf("parse error for env var %s: %w", envVar, err)
+			}
+
+		}
+	}
+
+	return nil
+}
---
+++ config_test.go
@@ -0,0 +1,222 @@
+package envconfig
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInt(t *testing.T) {
+	var stuff struct {
+		I int `env:"EC_INTERVAL" default:"100"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.I, 100)
+}
+
+func TestInt64(t *testing.T) {
+	var stuff struct {
+		I int64 `env:"EC_INTERVAL" default:"100"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, int(stuff.I), 100)
+}
+
+func TestInt32(t *testing.T) {
+	var stuff struct {
+		I int32 `env:"EC_INTERVAL" default:"100"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, int(stuff.I), 100)
+}
+
+func TestInt16(t *testing.T) {
+	var stuff struct {
+		I int16 `env:"EC_INTERVAL" default:"100"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, int(stuff.I), 100)
+}
+
+func TestInt8(t *testing.T) {
+	var stuff struct {
+		I int16 `env:"EC_INTERVAL" default:"100"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, int(stuff.I), 100)
+}
+
+func TestFloat64(t *testing.T) {
+	var stuff struct {
+		F float64 `env:"EC_INTERVAL" default:"10.3"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.F, float64(10.3))
+}
+
+func TestFloat32(t *testing.T) {
+	var stuff struct {
+		F float32 `env:"EC_INTERVAL" default:"10.3"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.F, float32(10.3))
+}
+
+func TestString(t *testing.T) {
+	var stuff struct {
+		S string `env:"EC_NAME" default:"hello"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.S, "hello")
+}
+
+func TestBoolTrueVariantT(t *testing.T) {
+	var stuff struct {
+		S bool `env:"EC_ENABLE" default:"t"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.S, true)
+}
+
+func TestBoolTrueVariantTrue(t *testing.T) {
+	var stuff struct {
+		S bool `env:"EC_ENABLE" default:"tRue"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.S, true)
+}
+
+func TestBoolTrueVariant1(t *testing.T) {
+	var stuff struct {
+		S bool `env:"EC_ENABLE" default:"1"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.S, true)
+}
+
+func TestBoolFalseVariantF(t *testing.T) {
+	var stuff struct {
+		S bool `env:"EC_ENABLE" default:"F"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.S, false)
+}
+
+func TestBoolFalseVariantFalse(t *testing.T) {
+	var stuff struct {
+		S bool `env:"EC_ENABLE" default:"False"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.S, false)
+}
+
+func TestBoolFalseVariant0(t *testing.T) {
+	var stuff struct {
+		S bool `env:"EC_ENABLE" default:"0"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.S, false)
+}
+
+func TestBoolFalseInvalid(t *testing.T) {
+	var stuff struct {
+		S bool `env:"EC_ENABLE" default:"boof"`
+	}
+	err := Apply(&stuff)
+	assert.Error(t, err)
+}
+
+func TestDurationWeek(t *testing.T) {
+	var stuff struct {
+		Dur time.Duration `env:"EC_LENGTH" default:"3w"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.Dur, 3*7*time.Hour*24)
+}
+
+func TestDurationDay(t *testing.T) {
+	var stuff struct {
+		Dur time.Duration `env:"EC_LENGTH" default:"2d"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.Dur, 2*time.Hour*24)
+}
+
+func TestDurationHour(t *testing.T) {
+	var stuff struct {
+		Dur time.Duration `env:"EC_LENGTH" default:"2h"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.Dur, 2*time.Hour)
+}
+
+func TestDurationMinute(t *testing.T) {
+	var stuff struct {
+		Dur time.Duration `env:"EC_LENGTH" default:"2m"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.Dur, 2*time.Minute)
+}
+
+func TestDurationSecond(t *testing.T) {
+	var stuff struct {
+		Dur time.Duration `env:"EC_LENGTH" default:"2s"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.Dur, 2*time.Second)
+}
+
+func TestDurationMillisecond(t *testing.T) {
+	var stuff struct {
+		Dur time.Duration `env:"EC_LENGTH" default:"2ms"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.Dur, 2*time.Millisecond)
+}
+
+func TestDurationMicrosecond(t *testing.T) {
+	var stuff struct {
+		Dur time.Duration `env:"EC_LENGTH" default:"2us"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.Dur, 2*time.Microsecond)
+}
+
+func TestDurationNanosecond(t *testing.T) {
+	var stuff struct {
+		Dur time.Duration `env:"EC_LENGTH" default:"2ns"`
+	}
+	err := Apply(&stuff)
+	assert.NoError(t, err)
+	assert.Equal(t, stuff.Dur, 2*time.Nanosecond)
+}
+
+func TestNoDefault(t *testing.T) {
+	var stuff struct {
+		F string `env:"EC_SOMETHING"`
+	}
+	err := Apply(&stuff, WithErrorOnMissing())
+	assert.Error(t, err)
+}
